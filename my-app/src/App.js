9df20d63aaf766c0300e69f745c8e608334cfddc
KaviPrajapati <kaviprajapati1997@gmail.com>
2019-09-27 21:25:33 +0530

    Made  signup page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Register from './components/Register';
 
 function App() {
   return (
-    <div className="App">
-      <nav class="navbar navbar-dark bg-dark">
-      <a class="navbar-brand" href="#">Navbar</a>
-      </nav>
-    </div>
+    <Router>
+    <React.Fragment>
+      <div className="App">
+        <nav class="navbar navbar-dark bg-dark">
+          <a class="navbar-brand" href="#">Navbar</a>
+        </nav>
+      </div>
+
+      <Route path="/register" component={Register}/>
+    </React.Fragment>
+  </Router>
   );
 }
 
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,14 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Register from './components/Register';
 
-ReactDOM.render(<Router>
-    <React.Fragment>
-        <div>
-            <Route exact path="/" component={App}/>
-        </div>
-    </React.Fragment>
-  </Router>, document.getElementById('root')   );
+ReactDOM.render(<App></App> , document.getElementById('root')   );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
