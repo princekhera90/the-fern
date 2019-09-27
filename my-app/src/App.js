c2a8069fe40837868ec34df86012136743237eb1
nikhil-jain655 <nik.jainpara@gmail.com>
2019-09-27 21:54:09 +0530

    update app.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import {BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Register from './components/Register';
 import Login from './login'
+import Dashboard from './dashboard';
 
 function App() {
   return (
@@ -12,13 +13,14 @@ function App() {
           <nav class="navbar navbar-dark bg-dark">
             <a class="navbar-brand" href="#">The Fern</a>
             <a class="nav-link" href="#">Login</a>
-            <a class="nav-link" href="#">Dashboard</a>
+            <a class="nav-link" href="/dashboard">Dashboard</a>
             <a class="nav-link" href="#">Profile</a>
           </nav>
         </div>
 
         <Route path="/login" component={Login}/>
         <Route path="/register" component={Register}/>
+        <Route path="/dashboard" component={Dashboard}/>
       </React.Fragment>
   </Router>)
 }
